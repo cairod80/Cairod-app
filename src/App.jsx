d9ff7aae6e9fac12c28eb920437217fa9f1a0603
cairod80 <themanshashan@gmail.com>
2026-07-23 05:52:23 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { useState, useEffect, useRef } from "react";
 import { createClient } from "@supabase/supabase-js";
 
+console.log("Vercel URL Check:", process.env.REACT_APP_SUPABASE_URL);
+console.log("Vercel Key Check:", process.env.REACT_APP_SUPABASE_ANON_KEY
+            
 const supabase = createClient(
   process.env.REACT_APP_SUPABASE_URL,
   process.env.REACT_APP_SUPABASE_ANON_KEY
